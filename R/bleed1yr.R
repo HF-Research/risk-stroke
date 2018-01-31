3e99dfd290a4bf4c8f6fd136f24aba9adcf13a45
Matthew Phelps <mPhelps@hjerteforeningen.dk>
2018-01-31 16:25:46 +0100

    Add bleed and stroke datasets

---
+++ R/bleed1yr.R
@@ -0,0 +1,31 @@
+#' One-year risk of bleeding
+#'
+#' A dataset containing the 1-year risk of bleeding for all combinations of the
+#' risk factors in untreated patients.
+#'
+#' @format A data frame with 2560 rows and 16 variables:
+#' \describe{
+#'   \item{year2000}{xxx}
+#'   \item{age}{Age of patient}
+#'   \item{bleeding}{xxx}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'
+#'   ...
+#' }
+#' @source \url{https://hjerteforeningen.dk/alt-om-dit-hjerte/}
+"bleed1yr"
+
---
+++ R/stroke1yr.R
@@ -0,0 +1,31 @@
+#' One-year risk of stroke
+#'
+#' A dataset containing the 1-year risk of stroke for all combinations of the
+#' risk factors in untreated patients.
+#'
+#' @format A data frame with 5120 rows and 16 variables:
+#' \describe{
+#'   \item{year2000}{xxx}
+#'   \item{age}{Age of patient}
+#'   \item{heartfailure}{xxx}
+#'   \item{hypertension}{}
+#'   \item{diabetes}{}
+#'   \item{vascular}{}
+#'   \item{female}{}
+#'   \item{stroke}{}
+#'   \item{chadsvasc}{}
+#'   \item{stroke1y}{}
+#'   \item{stroke1y.lower}{}
+#'   \item{stroke1y.upper}{}
+#'   \item{I63}{}
+#'   \item{I63.lower}{}
+#'   \item{I63.upper}{}
+#'   \item{thromb1y}{}
+#'   \item{thromb1y.lower}{}
+#'   \item{thromb1y.upper}{}
+#'
+#'   ...
+#' }
+#' @source \url{https://hjerteforeningen.dk/alt-om-dit-hjerte/}
+"stroke1yr"
+
