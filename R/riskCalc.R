4b8363559a187fabcc23eca4ceae4494fa5721e8
Matthew Phelps <mPhelps@hjerteforeningen.dk>
2018-03-27 13:33:50 +0200

    Add colored buttons
    Requires parsing the expression passed to radioButtonInput and adding quotation marks - this allows the jQuerry selector to select the appropriate element to apply CSS styling.

--- R/riskCalc.R
+++ R/riskCalc.R
@@ -11,6 +11,7 @@ riskCalc <- function(){
 # browser()
   stroke.dt <- data.table::data.table(stroke1yr)
 
+
   # Re-order columns to the order patients will enter their variables information
   # into the shiny app
   new.var.order <-  c("age",
@@ -34,6 +35,15 @@ riskCalc <- function(){
 
   setcolorder(stroke.dt,new.var.order)
   setkeyv(stroke.dt, new.var.order)
+
+  input_id_vec <- c("user_age",
+                    "sex",
+                    "stroke",
+                    "hf",
+                    "diabetes",
+                    "hyperT",
+                    "vasc")
+
   button.width <- "320px"
 
   shinyApp(
@@ -46,6 +56,7 @@ riskCalc <- function(){
 
       # Give CSS styles to selected elements
       tags$head(
+
         tags$style(type = "text/css", "#results {max-width:620px;}"),
         tags$style(type = "text/css", "#input_col {max-width:380px;}"), #max width input
         tags$style(type = "text/css", ".container-fluid {margin:auto; max-width: 1000px}"),
@@ -69,12 +80,12 @@ riskCalc <- function(){
                    inputId = "sex",
                    label = in_sex,
                    choices = c(
-                     "Unknown" = depSub(c("no", "yes")),
-                     "Male" = depSub("no"),
-                     "Female" = depSub("yes")
+                     "Unknown" = depSub(c(no, yes)),
+                     "Male" = depSub(no),
+                     "Female" = depSub(yes)
                    ),
                    checkIcon = list(yes = icon("check")),
-                   selected =  depSub(c("no", "yes")),
+                   selected =  depSub(c(no, yes)),
                    justified = TRUE,
                    width = button.width
                  ),
@@ -83,10 +94,10 @@ riskCalc <- function(){
                    inputId = "stroke",
                    label = in_stroke,
                    choiceNames = c(HTML("Unknown"), "No", "Yes"),
-                   selected =  depSub(c("no", "yes")),
-                   choiceValues = c(depSub(c("no", "yes")),
-                                    depSub("no"),
-                                    depSub("yes")),
+                   selected =  depSub(c(no, yes)),
+                   choiceValues = c(depSub(c(no, yes)),
+                                    depSub(no),
+                                    depSub(yes)),
                    checkIcon = list(yes = icon("check")),
                    justified = TRUE,
                    width = button.width
@@ -95,10 +106,10 @@ riskCalc <- function(){
                    inputId = "hf",
                    label = in_hf,
                    choiceNames = c(HTML("Unknown"), "No", "Yes"),
-                   selected =  depSub(c("no", "yes")),
-                   choiceValues = c(depSub(c("no", "yes")),
-                                    depSub("no"),
-                                    depSub("yes")),
+                   selected =  depSub(c(no, yes)),
+                   choiceValues = c(depSub(c(no, yes)),
+                                    depSub(no),
+                                    depSub(yes)),
                    checkIcon = list(yes = icon("check")),
                    justified = TRUE,
                    width = button.width
@@ -107,10 +118,10 @@ riskCalc <- function(){
                    inputId = "diabetes",
                    label = in_diab,
                    choiceNames = c(HTML("Unknown"), "No", "Yes"),
-                   selected =  depSub(c("no", "yes")),
-                   choiceValues = c(depSub(c("no", "yes")),
-                                    depSub("no"),
-                                    depSub("yes")),
+                   selected =  depSub(c(no, yes)),
+                   choiceValues = c(depSub(c(no, yes)),
+                                    depSub(no),
+                                    depSub(yes)),
                    checkIcon = list(yes = icon("check")),
                    justified = TRUE,
                    width = button.width
@@ -119,10 +130,10 @@ riskCalc <- function(){
                    inputId = "hyperT",
                    label = in_hyperT,
                    choiceNames = c(HTML("Unknown"), "No", "Yes"),
-                   selected =  depSub(c("no", "yes")),
-                   choiceValues = c(depSub(c("no", "yes")),
-                                    depSub("no"),
-                                    depSub("yes")),
+                   selected =  depSub(c(no, yes)),
+                   choiceValues = c(depSub(c(no, yes)),
+                                    depSub(no),
+                                    depSub(yes)),
                    checkIcon = list(yes = icon("check")),
                    justified = TRUE,
                    width = button.width
@@ -131,18 +142,26 @@ riskCalc <- function(){
                    inputId = "vasc",
                    label = in_vasc,
                    choiceNames = c(HTML("Unknown"), "No", "Yes"),
-                   selected =  depSub(c("no", "yes")),
-                   choiceValues = c(depSub(c("no", "yes")),
-                                    depSub("no"),
-                                    depSub("yes")),
+                   selected =  depSub(c(no, yes)),
+                   choiceValues = c(depSub(c(no, yes)),
+                                    depSub(no),
+                                    depSub(yes)),
                    checkIcon = list(yes = icon("check")),
                    justified = TRUE,
                    width = button.width
                  ),
-                 # tags$script(jsButtonColor("stroke", "#B2EDB5", depSub('no'))),
-                 tags$script(
-                   "$(\"input:radio[name='stroke'][value= no']\").parent().css('background-color', '#DE6B63');"
-                 )
+                 tags$script(jsButtonColor("stroke", "#B2EDB5", "no")),
+                 tags$script(jsButtonColor("hf", "#B2EDB5", "no")),
+                 tags$script(jsButtonColor("diabetes", "#B2EDB5", "no")),
+                 tags$script(jsButtonColor("hyperT", "#B2EDB5", "no")),
+                 tags$script(jsButtonColor("vasc", "#B2EDB5", "no")),
+
+                 tags$script(jsButtonColor("stroke", "#EDB6B2", "yes")),
+                 tags$script(jsButtonColor("hf", "#EDB6B2", "yes")),
+                 tags$script(jsButtonColor("diabetes", "#EDB6B2", "yes")),
+                 tags$script(jsButtonColor("hyperT", "#EDB6B2", "yes")),
+                 tags$script(jsButtonColor("vasc", "#EDB6B2", "yes"))
+
 
                )),
 
@@ -189,17 +208,39 @@ riskCalc <- function(){
         age_as_number <- txt2num()
         if (is.valid.age(age_as_number)) {
           # browser()
+          input_parse <- list()
+          for (i in input_id_vec[2:length(input_id_vec)]) {
 
+            input_parse[[i]] <- parse(text = input[[i]])
+            if (length(input_parse[[i]][[1]]) == 3) {
+              input_parse[[i]][[1]][[2]] <- "no"
+              input_parse[[i]][[1]][[3]] <- "yes"
+            } else if (input_parse[[i]][[1]] == "no") {
+              input_parse[[i]][[1]] <- "no"
+            } else {
+              input_parse[[i]][[1]] <- "yes"
+            }
+          }
 
           dat.sub <- stroke.dt[age == age_as_number &
-                                 female %in% evPar(input$sex) &
-                                 stroke %in% evPar(input$stroke) &
-                                 heartfailure %in% evPar(input$hf) &
-                                 diabetes %in% evPar(input$diabetes) &
-                                 hypertension %in% evPar(input$hyperT) &
-                                 vascular %in% evPar(input$vasc),
+                                 female %in% eval(input_parse[["sex"]]) &
+                                 stroke %in% eval(input_parse[["stroke"]]) &
+                                 heartfailure %in% eval(input_parse[["hf"]]) &
+                                 diabetes %in% eval(input_parse[["diabetes"]]) &
+                                 hypertension %in% eval(input_parse[["hyperT"]]) &
+                                 vascular %in% eval(input_parse[["vasc"]]),
                                range(stroke1y)]
 
+          test  <- stroke.dt[age == age_as_number &
+                                          female %in% eval(input_parse[["sex"]]) &
+                                          stroke %in% eval(input_parse[["stroke"]]) &
+                                          heartfailure %in% eval(input_parse[["hf"]]) &
+                                          diabetes %in% eval(input_parse[["diabetes"]]) &
+                                          hypertension %in% eval(input_parse[["hyperT"]]) &
+                                          vascular %in% eval(input_parse[["vasc"]]),
+                                        c(1,2,3,10)]
+
+          browser()
           one.value <- dat.sub[2] - dat.sub[1] < 0.001
           if (one.value) {
             paste0(dat.sub[1], "%")
---
+++ inst/www/script.js
@@ -0,0 +1,1 @@
+var test = function(par){ $("input:radio[name='stroke'][value= 'no']").parent().css('background-color', '#par');}
