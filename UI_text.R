8e85518f297d6d9c903516afb6f697c50a273f29
Matthew Phelps <ma.phelps@gmail.com>
2018-06-13 13:32:31 +0200

    Add help text and new results text

--- UI_text.R
+++ UI_text.R
@@ -9,68 +9,85 @@ subtitle_txt <- "Calculator of Absolute Stroke Risk"
 
 
 # INPUT UI ------------------------------------------------------------------
-in_age <- "Age of patient, choose between 20-99 years"
+input_title <- "1. INPUT"
+in_age <- "Patient's age (between 20-99 years)"
 in_sex <- "Patient's sex"
 
 in_stroke <- "Previous stroke/TCI/thromboembolism"
 in_hf <- "Heart failure"
 in_hyperT <- "Hypertension"
 in_diab <- "Diabetes mellitus"
-in_vasc <- "Vascular disease (Prior myocardial infarction, peripheral artery disease)"
+in_vasc <-
+  "Vascular disease (Prior myocardial infarction, peripheral artery disease)"
 
 
-help_stroke <- "Previous stroke / Transient Ischemic attack / thromboembolism"
+help_stroke <-
+  "Previous stroke / Transient Ischemic attack / thromboembolism"
 help_hf <- "Congestive heart failure/ Left ventricular dysfunction"
-help_hyperT <- "Treatment with antihypertensive drugs or resting sBP >= 140mmHg and/or
+help_hyperT <-
+  "Treatment with antihypertensive drugs or resting sBP >= 140mmHg and/or
 dBP>=90mmHg"
-help_diab <- "Treatment with oral hypoglycemic drugs or Glycated hemoglobin
+help_diab <-
+  "Treatment with oral hypoglycemic drugs or Glycated hemoglobin
 (HbA1C) ≥ 48 mmol/mol (≥ 6.5 DCCT %)"
-help_vasc <- "Prior myocardial infarction, peripheral artery disease"
+help_vasc <-
+  "Prior myocardial infarction, peripheral artery disease"
 # Must be in quotedt pixels (px), e.g.: "100px"
-in_txt_size <- "14px"
 
 
 # OUTPUT TEXT -------------------------------------------------------------
 
-results_title <- "RESULTS"
-out_stroke <- "The average 1-year risk of stroke and thromboembolism:"
+results_title <- "2. RESULTS"
+out_stroke <-
+  "The risk of having a stroke and thromboembolism within one-year following
+atrial fibrillation diagnosis is:"
+
+out_stroke2 <- "This means that "
+out_stroke3 <- " out of 1000 patients with the same risk
+factors would experience a stroke within the year following diagnisis"
+out_strokeLessOne <- "This means that <1 out of every 1000 patients with the same
+risk factors would experience a stroke within the year following diagnosis"
 
 out_stroke_details <- "
 CARS calculates the absolute 1-year risk of stroke and thromboembolism for patients
 with atrial fibrillation and not receiving anticoagulation.
 The calculator is based on a large cohort of Danish patients admitted first time to the hospital
-for their atrial fibrillation (1)."
+for their atrial fibrillation [1]."
 
 
 out_stroke_details2 <- "
-The calculator uses the same risk factors as the CHA2DS2-VASc score (2) recommended in the latest European/American guidelines
-to guide the need for anticoagulation for patients with atrial fibrillation (3-4).
+The calculator uses the same risk factors as the CHA2DS2-VASc score [2] recommended in the latest European/American guidelines
+to guide the need for anticoagulation for patients with atrial fibrillation [3-4].
 The main differences are the following:"
 out_details_list1 <- "Age is included as a continuous variable"
-out_details_list2 <- "The overall risk calculation takes into account not a sum of risk factors, but their individual contribution."
+out_details_list2 <-
+  "The overall risk calculation takes into account not a sum of risk factors, but their individual contribution."
 #This estimate is based on X number of patients who had similar covariates
 out_stroke_details2_size <- "14px"
 
 references <- "References:"
 references_size <- "12px"
 
-ref1 <- "1. Lee J-Y.C, Ozenne B, Gislason G, Olesen JB, Torp-Pedersen C, Gerds TA.
+ref1 <-
+  "Lee J-Y.C, Ozenne B, Gislason G, Olesen JB, Torp-Pedersen C, Gerds TA.
 The individual risk of stroke in patients with atrial fibrillation by the CHADSVASC score."
 ref_size <- "12px"
 
-ref2 <-" 2. 	Kirchhof P, Benussi S, Kotecha D, Ahlsson A, Atar D, Casadei B, m.fl.
+ref2 <-
+  "Kirchhof P, Benussi S, Kotecha D, Ahlsson A, Atar D, Casadei B, m.fl.
 2016 ESC Guidelines for the management of atrial fibrillation developed in collaboration with EACTS.
 Eur Heart J. 7. October 2016;37(38):2893–962."
 
 
-ref3 <-" 3. 	January CT, Wann LS, Alpert JS, Calkins H, Cigarroa JE, Cleveland JC, m.fl.
+ref3 <-
+  "January CT, Wann LS, Alpert JS, Calkins H, Cigarroa JE, Cleveland JC, m.fl.
 2014 AHA/ACC/HRS Guideline for the Management of Patients With Atrial Fibrillation:
 Executive Summary: A Report of the American College of Cardiology/
 American Heart Association Task Force on Practice Guidelines and the Heart Rhythm Society.
 J Am Coll Cardiol. 2. December 2014;64(21):2246–80."
 
 
-ref4 <-"4. 	Lip GYH, Nieuwlaat R, Pisters R, Lane DA, Crijns HJGM.
+ref4 <- "Lip GYH, Nieuwlaat R, Pisters R, Lane DA, Crijns HJGM.
 Refining clinical risk stratification for predicting stroke and thromboembolism in atrial fibrillation
 using a novel risk factor-based approach: the euro heart survey on atrial fibrillation.
 Chest. February 2010;137(2):263-72."
@@ -79,4 +96,5 @@ Chest. February 2010;137(2):263-72."
 
 # Text that appears when user has not entered an appropriate age (i.e. <20 ||
 # >99 || not a whole number)
-enter_age <- "Please enter a whole number (years of age) between 20 - 99"
+enter_age <-
+  "Please enter patient's age (between 20 - 99) in the input panel"
