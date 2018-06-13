ceee002b6d0855a4c8b859cfcc7ea6aa61e59905
Matthew Phelps <ma.phelps@gmail.com>
2018-06-13 13:31:30 +0200

    Modify results section
    Break results into two sections and modify the condition for showing the "enter age" text

--- app.R
+++ app.R
@@ -9,7 +9,6 @@ source("UI_text.R")
 # browser()
 stroke.dt <- data.table::data.table(stroke1yr)
 
-
 # Re-order columns to the order patients will enter their variables information
 # into the shiny app
 new.var.order <-  c(
@@ -55,19 +54,8 @@ ui <- fluidPage(
 
 
   tags$head(
-    # Main styling is in CSS file:
-    # tags$link(rel = "stylesheet", type = "text/css", href = "bootswatch-yeti.css"),
-    # tags$link(rel = "stylesheet", type = "text/css", href = "materia.css"),
-    tags$link(rel = "stylesheet", type = "text/css", href = "cars-style.css"),
-
-
-    # Styling that's linked to UI_text file goes here:
-    tags$style(paste0(".ref {font-size:", ref_size, "}")),
-    tags$style(paste0(
-      "#references {font-size:", references_size, "}"
-    )) # font size
-    ),
-
+    tags$link(rel = "stylesheet", type = "text/css", href = "cars-style.css")
+  ),
 
   fluidRow(
     # Input column
@@ -75,11 +63,14 @@ ui <- fluidPage(
       id = "input_col",
       6,
       wellPanel(
-        textInput(
+        div(id = "input_title", input_title),
+        br(),
+        numericInput(
           inputId = "user_age",
           label = in_age,
-          value = NULL,
-          placeholder = "Age"
+          min = 20,
+          max = 99,
+          value = "Age"
         ),
         shinyWidgets::radioGroupButtons(
           inputId = "sex",
@@ -163,29 +154,34 @@ ui <- fluidPage(
       fluidRow(wellPanel(
         id = "results_well",
         div(id = "results_title", results_title),
-        # div(id = "out_intro", out_intro),
+        div(class = "output_desc", textOutput("output_description1")),
+        tags$h1(strong(textOutput("strokeRisk"))),
         br(),
-        div(id = "stroke_results_text", out_stroke),
-        tags$h1(strong(textOutput("strokeRisk")))
+        div(class = "output_desc", textOutput("output_description2"))
+
       )),
       div(class = "out_stroke_details", out_stroke_details),
       br(),
       div(class = "out_stroke_details", out_stroke_details2),
-      tags$ul(class = "out_stroke_details",
-              tags$li(out_details_list1),
-              tags$li(out_details_list2)
+      tags$ul(
+        class = "out_stroke_details",
+        tags$li(out_details_list1),
+        tags$li(out_details_list2)
       ),
       br(),
       br(),
       hr(),
-      div(id = "references", references),
-      div(class = "ref", ref1),
-      div(class = "ref", ref2),
-      div(class = "ref", ref3),
-      div(class = "ref", ref4)
+      div(class = "ref", references),
+      tags$ol(
+        class = "ref",
+        tags$li(ref1),
+        tags$li(ref2),
+        tags$li(ref3),
+        tags$li(ref4)
+      )
     )
   )
-  )
+)
 
 
 # SERVER ------------------------------------------------------------------
@@ -200,10 +196,9 @@ server <- function(input, output) {
     # browser()
     age_as_number <- txt2num()
     if (!is.valid.age(age_as_number)) {
-      paste0(enter_age)
+      enter_age
 
     } else {
-      # browser()
       dat.sub <- stroke.dt[age == age_as_number &
                              female %in% input$sex &
                              stroke %in% input$stroke &
@@ -211,28 +206,47 @@ server <- function(input, output) {
                              diabetes %in% input$diabetes &
                              hypertension %in% input$hyperT &
                              vascular %in% input$vasc,
-                           range(stroke1y)]
+                           stroke1y]
+      ifelse(dat.sub < 0.1,
+             paste0("<0.1%"),
+             paste0(sprintf(
+               "%.1f",
+               round(dat.sub[1],
+                     digits = 1)),
+             "%"))
+    }
 
-      test  <- stroke.dt[age == age_as_number &
-                           female %in% input$sex &
-                           stroke %in% input$stroke &
-                           heartfailure %in% input$hf &
-                           diabetes %in% input$diabetes &
-                           hypertension %in% input$hyperT &
-                           vascular %in% input$vasc,
-                         c(3, 7, 10)]
+  })
+  output$output_description1 <- renderText({
+    # browser()
+    age_as_number <- txt2num()
+    if (is.valid.age(age_as_number)) {
+      out_stroke
+    } else {
+      ""
+    }
 
-      one.value <- dat.sub[2] - dat.sub[1] < 0.001
-      if (one.value) {
-        paste0(dat.sub[1], "%")
-      } else if (!one.value) {
-        paste0("between ", dat.sub[1],
-               "%", " and ",
-               dat.sub[2],
-               "%")
-      }
+  })
+  output$output_description2 <- renderText({
+    # browser()
+    age_as_number <- txt2num()
+    if (is.valid.age(age_as_number)) {
+      dat.sub <- stroke.dt[age == age_as_number &
+                             female %in% input$sex &
+                             stroke %in% input$stroke &
+                             heartfailure %in% input$hf &
+                             diabetes %in% input$diabetes &
+                             hypertension %in% input$hyperT &
+                             vascular %in% input$vasc,
+                           stroke1y]
+      number_strokes <- round(dat.sub*0.01*1000)
+      ifelse(number_strokes >= 1,
+             paste0(out_stroke2, number_strokes, out_stroke3),
+             out_strokeLessOne
+             )
 
-
+    } else {
+      ""
     }
 
   })
