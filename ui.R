eeb932d87196dca0141fa078cc80e0a871de552e
Matthew Phelps <ma.phelps@gmail.com>
2018-07-05 13:56:29 +0200

    Split app file into ui and server files

--- ui.R
+++ ui.R
@@ -1,8 +1,6 @@
 library(shiny)
-# library(DT)
 library(shinyWidgets)
 library(data.table)
-
 
 source("functions.R")
 source("UI_text.R")
@@ -16,8 +14,8 @@ input_id_vec <- c("user_age",
                   "hyperT",
                   "vasc")
 button.width <- "260px"
-
-ui <- fluidPage(
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
   # Application title
   h1(title_txt),
   h4(subtitle_txt),
@@ -212,130 +210,4 @@ ui <- fluidPage(
     )
   )
 )
-
-
-# SERVER ------------------------------------------------------------------
-server <- function(input, output) {
-  txt2num <- reactive({
-    as.numeric(input$user_age)
-  })
-
-  subsetStroke <- reactive({
-    age_as_number <- txt2num()
-    stroke_sub <- stroke.dt[age == age_as_number &
-                              female %in% input$sex &
-                              stroke %in% input$stroke &
-                              heartfailure %in% input$hf &
-                              diabetes %in% input$diabetes &
-                              hypertension %in% input$hyperT &
-                              vascular %in% input$vasc,
-                            stroke1y]
-    out <- list(age_as_number = age_as_number,
-                stroke_sub = stroke_sub)
-  })
-
-  subsetBleed <- reactive({
-    age_as_number <- txt2num()
-    # browser()
-    bleed_sub <- bleed.dt[age == age_as_number &
-                            hypertension %in% input$hyperT &
-                            stroke %in% input$stroke &
-                            ckd %in% input$ckd &
-                            bleeding %in% input$bleeding &
-                            drugs %in% input$drugs,
-                          bleeding1y]
-    list(age_as_number = age_as_number,
-         bleed_sub = bleed_sub)
-  })
-
-  output$strokeRisk <- renderText({
-    # Order of subset arguments must be same order as new.col.order variable set in
-    # intro.
-    # browser()
-    stroke_sub <- subsetStroke()
-    if (!is.valid.age(stroke_sub$age_as_number)) {
-      enter_age
-    } else {
-      ifelse(stroke_sub$stroke_sub < 0.1,
-             paste0("<0.1%"),
-             paste0(sprintf(
-               "%.1f",
-               round(stroke_sub$stroke_sub,
-                     digits = 1)
-             ),
-             "%"))
-    }
-  })
-
-  output$stroke_desc1 <- renderText({
-    # browser()
-    age_as_number <- txt2num()
-    if (is.valid.age(age_as_number)) {
-      out_stroke
-    } else {
-      ""
-    }
-  })
-
-  output$stroke_desc2 <- renderText({
-    # browser()
-    stroke_sub <- subsetStroke()
-    if (is.valid.age(stroke_sub$age_as_number)) {
-      number_strokes <-
-        (round(stroke_sub$stroke_sub, digits = 1) * 0.01 * 1000)
-      ifelse(
-        number_strokes >= 1,
-        paste0(out_stroke2, number_strokes, out_stroke3),
-        out_strokeLessOne
-      )
-    } else {
-      ""
-    }
-  })
-
-  output$bleedRisk <- renderText({
-    # browser()
-    bleed_sub <- subsetBleed()
-    if (!is.valid.age(bleed_sub$age_as_number)) {
-      enter_age
-    } else {
-      ifelse(bleed_sub$bleed_sub < 0.1,
-             paste0("<0.1%"),
-             paste0(sprintf(
-               "%.1f",
-               round(bleed_sub$bleed_sub,
-                     digits = 1)
-             ),
-             "%"))
-    }
-  })
-
-  output$bleed_desc1 <- renderText({
-    # browser()
-    age_as_number <- txt2num()
-    if (is.valid.age(age_as_number)) {
-      out_bleeding1
-    } else {
-      ""
-    }
-  })
-
-  output$bleed_desc2 <- renderText({
-    # browser()
-    bleed_sub <- subsetBleed()
-    if (is.valid.age(bleed_sub$age_as_number)) {
-      number_bleed <-
-        (round(bleed_sub$bleed_sub, digits = 1) * 0.01 * 1000)
-      ifelse(
-        number_bleed >= 1,
-        paste0(out_bleeding2, number_bleed, out_bleeding3),
-        out_bleedingLessOne
-      )
-    } else {
-      ""
-    }
-  })
-
-}
-
-shinyApp(ui = ui, server = server)
+)
