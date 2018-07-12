9c5b8e45ee120333859329c41f340527acb74723
Matthew Phelps <ma.phelps@gmail.com>
2018-07-12 11:08:31 +0200

    Clean up reactive functions. Slightly improve efficieny. Since reactives cache their values until the input changes, I don't have to worry about multiple calls to those functions

--- server.R
+++ server.R
@@ -1,53 +1,52 @@
 library(shiny)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
+
+# REACTIVE FUNCTIONS ------------------------------------------------------
   txt2num <- reactive({
     as.numeric(input$user_age)
   })
 
   subsetStroke <- reactive({
-    age_as_number <- txt2num()
-    stroke_sub <- stroke.dt[age == age_as_number &
-                              female %in% input$sex &
-                              stroke %in% input$stroke &
-                              heartfailure %in% input$hf &
-                              diabetes %in% input$diabetes &
-                              hypertension %in% input$hyperT &
-                              vascular %in% input$vasc,
-                            stroke1y]
-    out <- list(age_as_number = age_as_number,
-                stroke_sub = stroke_sub)
+    # Reactive function will save it's output. It will only run again if input
+    # has changed, otherwise, repeated calls to it will access the cached values
+    # only
+    stroke.dt[age == txt2num() &
+                female %in% input$sex &
+                stroke %in% input$stroke &
+                heartfailure %in% input$hf &
+                diabetes %in% input$diabetes &
+                hypertension %in% input$hyperT &
+                vascular %in% input$vasc,
+              stroke1y]
   })
 
   subsetBleed <- reactive({
-    age_as_number <- txt2num()
-    # browser()
-    bleed_sub <- bleed.dt[age == age_as_number &
-                            hypertension %in% input$hyperT &
-                            stroke %in% input$stroke &
-                            ckd %in% input$ckd &
-                            bleeding %in% input$bleeding &
-                            drugs %in% input$drugs,
-                          bleeding1y]
-    list(age_as_number = age_as_number,
-         bleed_sub = bleed_sub)
+    bleed.dt[age == txt2num() &
+               hypertension %in% input$hyperT &
+               stroke %in% input$stroke &
+               ckd %in% input$ckd &
+               bleeding %in% input$bleeding &
+               drugs %in% input$drugs,
+             bleeding1y]
   })
 
+
+
+# WRITE STROKE OUTPUT -----------------------------------------------------
   output$strokeRisk <- renderText({
     # Order of subset arguments must be same order as new.col.order variable set in
     # intro.
-    # browser()
-    stroke_sub <- subsetStroke()
-    if (!is.valid.age(stroke_sub$age_as_number)) {
+
+    if (!is.valid.age(txt2num())) {
       enter_age
     } else {
-      ifelse(stroke_sub$stroke_sub < 0.1,
+      ifelse(subsetStroke() < 0.1,
              paste0("<0.1%"),
              paste0(sprintf(
                "%.1f",
-               round(stroke_sub$stroke_sub,
+               round(subsetStroke(),
                      digits = 1)
              ),
              "%"))
@@ -55,21 +54,15 @@ shinyServer(function(input, output) {
   })
 
   output$stroke_desc1 <- renderText({
-    # browser()
-    age_as_number <- txt2num()
-    if (is.valid.age(age_as_number)) {
-      out_stroke
-    } else {
-      ""
-    }
+    ifelse(is.valid.age(txt2num()), out_stroke, "")
   })
 
   output$stroke_desc2 <- renderText({
     # browser()
-    stroke_sub <- subsetStroke()
-    if (is.valid.age(stroke_sub$age_as_number)) {
+
+    if (is.valid.age(txt2num())) {
       number_strokes <-
-        (round(stroke_sub$stroke_sub, digits = 1) * 0.01 * 1000)
+        (round(subsetStroke(), digits = 1) * 0.01 * 1000)
       ifelse(
         number_strokes >= 1,
         paste0(out_stroke2, number_strokes, out_stroke3),
@@ -80,17 +73,17 @@ shinyServer(function(input, output) {
     }
   })
 
+# WRITE BLEEDING OUTPUT ---------------------------------------------------
   output$bleedRisk <- renderText({
     # browser()
-    bleed_sub <- subsetBleed()
-    if (!is.valid.age(bleed_sub$age_as_number)) {
+    if (!is.valid.age(txt2num())) {
       enter_age
     } else {
-      ifelse(bleed_sub$bleed_sub < 0.1,
+      ifelse(subsetBleed() < 0.1,µ
              paste0("<0.1%"),
              paste0(sprintf(
                "%.1f",
-               round(bleed_sub$bleed_sub,
+               round(subsetBleed(),
                      digits = 1)
              ),
              "%"))
@@ -98,21 +91,13 @@ shinyServer(function(input, output) {
   })
 
   output$bleed_desc1 <- renderText({
-    # browser()
-    age_as_number <- txt2num()
-    if (is.valid.age(age_as_number)) {
-      out_bleeding1
-    } else {
-      ""
-    }
+    ifelse(is.valid.age(txt2num()), out_bleeding1, "")
   })
 
   output$bleed_desc2 <- renderText({
-    # browser()
-    bleed_sub <- subsetBleed()
-    if (is.valid.age(bleed_sub$age_as_number)) {
+    if (is.valid.age(txt2num())) {
       number_bleed <-
-        (round(bleed_sub$bleed_sub, digits = 1) * 0.01 * 1000)
+        (round(subsetBleed(), digits = 1) * 0.01 * 1000)
       ifelse(
         number_bleed >= 1,
         paste0(out_bleeding2, number_bleed, out_bleeding3),
