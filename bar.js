180a0ce5151f55b8c479cdc4564421cb79cc61d8
Matthew Phelps <ma.phelps@gmail.com>
2018-07-26 14:38:31 +0200

    Add d3 bar and scatterplot as sandbox testing scripts for learning d3. Can remove later

--- bar.js
+++ bar.js
@@ -1,41 +1,59 @@
-     // !preview r2d3 data=data2json(df)
+// !preview r2d3 data=c(0.3, 0.6, 0.8, 0.95, 0.40, 0.20)
+//
+// r2d3: https://rstudio.github.io/r2d3
+//
 
-var ySpace = Math.floor(height / data.length);
-var xSpace = Math.floor(width / data.length);
+// Help from: https://www.visualcinnamon.com/2016/05/smooth-color-legend-d3-svg-gradient.html
+var barHeight = Math.ceil(height / data.length);
 
-var pad = 20;
-
-var scaleX = d3.scaleLinear() //scaleLinear in d3v4!
-  .domain([0, d3.max(data, function(d){return d[0];})])
-  .range([pad, width - pad]);
-
-var scaleY = d3.scaleLinear()
-  .domain([0, d3.max(data, function(d){return d[1];})])
-  .range([height - pad, pad]);
-
-var scaleR = d3.scaleLinear()
-  .domain([0, d3.max(data, function(d){return d[1];})])
-  .range([1, 10]);
-
-var axisX = d3.axisBottom(scaleX);
-
-
-svg.selectAll('circle')
+svg.selectAll('rect')
   .data(data)
-  .enter().append('circle')
-    .attr("cx", function(d, i){
-      return scaleX(d[0]);
+  .enter().append('rect')
+    .attr('width', function(d) { return d * width; })
+    .attr('height', barHeight)
+    .attr('y', function(d, i) { return i * barHeight; })
+    .attr('fill', 'steelblue');
 
-    })
-    .attr("cy", function(d, i){
-      return scaleY(d[1]);
-    })
-    .attr('r', function(d){
-      return scaleR(d[1]);
-    })
-    .style('fill', "steelblue");
+////Append a defs (for definition) element to your SVG
+var defs = svg.append("defs");
 
-svg.append("g")
-  .call(axisX)
-  .attr("transform", "translate(0," + (height - pad) + ")");
+var linearGradient = defs.append("linearGradient")
+  .attr("id", "linear-gradient");
 
+// Horizontal gradient
+linearGradient
+  .attr("x1", "0%")
+  .attr("y1", "0%")
+  .attr("x2", "100%")
+  .attr("y2", "0%");
+
+// Set color for start (0%)
+linearGradient.append("stop")
+  .attr("offset", "0%")
+  .attr("stop-color", "green");
+
+// Set color for end (100%)
+linearGradient.append("stop")
+  .attr("offset", "100%")
+  .attr("stop-color", "#8b0000");
+
+svg.append("rect")
+  .attr("width", width)
+  .attr("height", 10)
+  .style("fill", "url(#linear-gradient");
+
+
+//set up needle
+var needle = svg.selectAll( ".needle" )
+	.data( [0] )
+	.enter()
+	.append( 'line' )
+	.attr( "x1", 0 )
+	.attr( "x2", -78 )
+	.attr( "y1", 0 )
+	.attr( "y2", 0 )
+	.classed("needle", true)
+	.style( "stroke", "black" )
+	.attr( "transform", function( d ) {
+		return " translate(200,200) rotate(" + d + ")"
+	} );
