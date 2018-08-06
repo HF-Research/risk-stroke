3571e1958532795883dfc655952d836a295d3edd
Matthew Phelps <ma.phelps@gmail.com>
2018-08-06 10:05:27 +0200

    Add NavBars for doctor and patient views.
    NavBars means the user has to re-input variables when switching to a new-view

--- ui.R
+++ ui.R
@@ -10,223 +10,220 @@ source("1-data-prep.R")
 
 button.width <- "260px"
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  useShinyjs(),
-  # Application title
-  h1(title_txt),
-  h4(subtitle_txt),
-  br(),
-  # theme = shinythemes::shinytheme(theme = "yeti"),
+shinyUI(ui = tagList(tags$head(
+  tags$link(rel = "stylesheet", type = "text/css", href = "cars-style.css")
+),
 
+navbarPage(
+  title_txt,
+  tabPanel("Læge visning",
 
-  tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "cars-style.css")
-  ),
+           fluidRow(
+             # INPUT -------------------------------------------------------------------
+             column(
+               id = "input_col",
+               6,
+               wellPanel(
+                 div(class = "titles", input_title),
+                 br(),
 
-  fluidRow(
-    # INPUT -------------------------------------------------------------------
-    column(
-      id = "input_col",
-      6,
-      wellPanel(
-        div(class = "titles", input_title),
-        br(),
+                 numericInput(
+                   inputId = "user_age",
+                   label = in_age,
+                   min = 20,
+                   max = 99,
+                   value = "Age"
+                 ),
 
-        numericInput(
-          inputId = "user_age",
-          label = in_age,
-          min = 20,
-          max = 99,
-          value = "Age"
-        ),
+                 shinyWidgets::radioGroupButtons(
+                   inputId = "sex",
+                   label = in_sex,
+                   choices = c("Mand" = "no",
+                               "Kvinde" = "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   selected =  "no",
+                   # width = button.width,
+                   justified = TRUE
+                 ),
 
-        shinyWidgets::radioGroupButtons(
-          inputId = "sex",
-          label = in_sex,
-          choices = c("Mand" = "no",
-                      "Kvinde" = "yes"),
-          checkIcon = list(yes = icon("check")),
-          selected =  "no",
-          # width = button.width,
-          justified = TRUE
-        ),
+                 br(),
+                 div(id = "in_prevDiag", in_prevDiag),
 
-        br(),
-        div(id = "in_prevDiag", in_prevDiag),
+                 shinyWidgets::radioGroupButtons(
+                   inputId = "hf",
+                   label = in_hf,
+                   choiceNames = in_yesNo_choice,
+                   selected =  ("no"),
+                   choiceValues = c("no",
+                                    "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   # width = button.width,
+                   justified = TRUE
+                 ),
 
-        shinyWidgets::radioGroupButtons(
-          inputId = "hf",
-          label = in_hf,
-          choiceNames = in_yesNo_choice,
-          selected =  ("no"),
-          choiceValues = c("no",
-                           "yes"),
-          checkIcon = list(yes = icon("check")),
-          # width = button.width,
-          justified = TRUE
-        ),
+                 shinyWidgets::radioGroupButtons(
+                   inputId = "diabetes",
+                   label = in_diab,
 
-        shinyWidgets::radioGroupButtons(
-          inputId = "diabetes",
-          label = in_diab,
+                   choiceNames = in_yesNo_choice,
+                   selected =  ("no"),
+                   choiceValues = c("no",
+                                    "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   # width = button.width,
+                   justified = TRUE
+                 ),
 
-          choiceNames = in_yesNo_choice,
-          selected =  ("no"),
-          choiceValues = c("no",
-                           "yes"),
-          checkIcon = list(yes = icon("check")),
-          # width = button.width,
-          justified = TRUE
-        ),
+                 shinyWidgets::radioGroupButtons(
+                   inputId = "vasc",
+                   label = in_vasc,
+                   choiceNames = in_yesNo_choice,
+                   selected =  ("no"),
+                   choiceValues = c("no",
+                                    "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   # width = button.width,
+                   justified = TRUE
+                 ),
 
-        shinyWidgets::radioGroupButtons(
-          inputId = "vasc",
-          label = in_vasc,
-          choiceNames = in_yesNo_choice,
-          selected =  ("no"),
-          choiceValues = c("no",
-                           "yes"),
-          checkIcon = list(yes = icon("check")),
-          # width = button.width,
-          justified = TRUE
-        ),
+                 ## BOTH variables:
+                 hr(),
 
-        ## BOTH variables:
-        hr(),
+                 shinyWidgets::radioGroupButtons(
+                   inputId = "stroke",
+                   label = in_stroke,
+                   choiceNames = in_yesNo_choice,
+                   selected =  "no",
+                   choiceValues = c("no",
+                                    "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   # width = button.width,
+                   justified = TRUE
+                 ),
 
-        shinyWidgets::radioGroupButtons(
-          inputId = "stroke",
-          label = in_stroke,
-          choiceNames = in_yesNo_choice,
-          selected =  "no",
-          choiceValues = c("no",
-                           "yes"),
-          checkIcon = list(yes = icon("check")),
-          # width = button.width,
-          justified = TRUE
-        ),
-
-        shinyWidgets::radioGroupButtons(
-          inputId = "hyperT",
-          label = in_hyperT,
-          choiceNames = in_yesNo_choice,
-          selected =  ("no"),
-          choiceValues = c("no",
-                           "yes"),
-          checkIcon = list(yes = icon("check")),
-          # width = button.width,
-          justified = TRUE
-        ),
-
-
-        ## BLEEDING only variables
-        hr(),
-
-        shinyWidgets::radioGroupButtons(
-          inputId = "bleeding",
-          label = in_bleed,
-          choiceNames = in_yesNo_choice,
-          selected =  ("no"),
-          choiceValues = c("no",
-                           "yes"),
-          checkIcon = list(yes = icon("check")),
-          # width = button.width,
-          justified = TRUE
-        ),
-
-        shinyWidgets::radioGroupButtons(
-          inputId = "ckd",
-          label = in_ckd,
-          choiceNames = in_yesNo_choice,
-          selected =  ("no"),
-          choiceValues = c("no",
-                           "yes"),
-          checkIcon = list(yes = icon("check")),
-          # width = button.width,
-          justified = TRUE
-        ),
-
-        shinyWidgets::radioGroupButtons(
-          inputId = "drugs",
-          label = in_drug,
-          choiceNames = in_yesNo_choice,
-          selected =  ("no"),
-          choiceValues = c("no",
-                           "yes"),
-          checkIcon = list(yes = icon("check")),
-          # width = button.width,
-          justified = TRUE
-        )
-      )
-    ),
-
-
-    # OUTPUT ------------------------------------------------------------------
-
-    column(
-      id = "results",
-      6,
-      tabsetPanel(
-        type = "tabs",
-        tabPanel("text",
-                 fluidRow(
-                   wellPanel(
-                     id = "results_stroke_well",
-                     div(class = "titles", results_stroke_title),
-                     div(class = "output_desc", textOutput("stroke_desc1")),
-                     tags$h1(strong(textOutput("strokeRisk"))),
-                     br(),
-                     div(class = "output_desc", textOutput("stroke_desc2"))
-                   )
+                 shinyWidgets::radioGroupButtons(
+                   inputId = "hyperT",
+                   label = in_hyperT,
+                   choiceNames = in_yesNo_choice,
+                   selected =  ("no"),
+                   choiceValues = c("no",
+                                    "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   # width = button.width,
+                   justified = TRUE
                  ),
 
 
-                 fluidRow(
-                   wellPanel(
-                     id = "results_bleeding_well",
-                     div(class = "titles", results_bleeding_title),
-                     div(class = "output_desc", textOutput("bleed_desc1")),
-                     tags$h1(strong(textOutput("bleedRisk"))),
-                     br(),
-                     div(class = "output_desc", textOutput("bleed_desc2"))
+                 ## BLEEDING only variables
+                 hr(),
 
-                   )
-                 )),
-
-        tabPanel("plot",
-                 fluidRow(
-                   wellPanel(id = "results_gauge",
-                             d3Output("plot_riskbar", height = "100%"))
+                 shinyWidgets::radioGroupButtons(
+                   inputId = "bleeding",
+                   label = in_bleed,
+                   choiceNames = in_yesNo_choice,
+                   selected =  ("no"),
+                   choiceValues = c("no",
+                                    "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   # width = button.width,
+                   justified = TRUE
                  ),
 
+                 shinyWidgets::radioGroupButtons(
+                   inputId = "ckd",
+                   label = in_ckd,
+                   choiceNames = in_yesNo_choice,
+                   selected =  ("no"),
+                   choiceValues = c("no",
+                                    "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   # width = button.width,
+                   justified = TRUE
+                 ),
 
-                 fluidRow(
-                   wellPanel(id = "results_plot_stroke",
-                             uiOutput("plot_stroke"))
+                 shinyWidgets::radioGroupButtons(
+                   inputId = "drugs",
+                   label = in_drug,
+                   choiceNames = in_yesNo_choice,
+                   selected =  ("no"),
+                   choiceValues = c("no",
+                                    "yes"),
+                   checkIcon = list(yes = icon("check")),
+                   # width = button.width,
+                   justified = TRUE
                  )
-                 )),
-
-        div(class = "out_stroke_details", out_stroke_details),
-        br(),
-        div(class = "out_stroke_details", out_stroke_details2),
-        tags$ul(
-          class = "out_stroke_details",
-          tags$li(out_details_list1),
-          tags$li(out_details_list2)
-        ),
-        br(),
-        br(),
-        hr(),
-        div(class = "ref", references),
-        tags$ol(
-          class = "ref",
-          tags$li(ref1),
-          tags$li(ref2),
-          tags$li(ref3),
-          tags$li(ref4)
-        )
+               )
+             ),
 
 
-    )
-  )
+             # OUTPUT ------------------------------------------------------------------
+
+             column(
+               id = "results",
+               6,
+               tabsetPanel(
+                 type = "tabs",
+                 tabPanel("text",
+                          fluidRow(
+                            wellPanel(
+                              id = "results_stroke_well",
+                              div(class = "titles", results_stroke_title),
+                              div(class = "output_desc", textOutput("stroke_desc1")),
+                              tags$h1(strong(textOutput("strokeRisk"))),
+                              br(),
+                              div(class = "output_desc", textOutput("stroke_desc2"))
+                            )
+                          ),
+
+
+                          fluidRow(
+                            wellPanel(
+                              id = "results_bleeding_well",
+                              div(class = "titles", results_bleeding_title),
+                              div(class = "output_desc", textOutput("bleed_desc1")),
+                              tags$h1(strong(textOutput("bleedRisk"))),
+                              br(),
+                              div(class = "output_desc", textOutput("bleed_desc2"))
+
+                            )
+                          )),
+
+                 tabPanel("plot",
+                          fluidRow(
+                            wellPanel(id = "results_gauge",
+                                      d3Output("plot_riskbar", height = "100%"))
+                          ),
+
+
+                          fluidRow(
+                            wellPanel(id = "results_plot_stroke",
+                                      uiOutput("plot_stroke"))
+                          ))
+               ),
+
+               div(class = "out_stroke_details", out_stroke_details),
+               br(),
+               div(class = "out_stroke_details", out_stroke_details2),
+               tags$ul(
+                 class = "out_stroke_details",
+                 tags$li(out_details_list1),
+                 tags$li(out_details_list2)
+               ),
+               br(),
+               br(),
+               hr(),
+               div(class = "ref", references),
+               tags$ol(
+                 class = "ref",
+                 tags$li(ref1),
+                 tags$li(ref2),
+                 tags$li(ref3),
+                 tags$li(ref4)
+               )
+
+
+             )
+           )),
+  tabPanel("Patient view")
+)
 ))
