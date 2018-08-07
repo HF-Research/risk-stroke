7a08317e6b13df73e02b2dead793f6f2883b0a0e
Matthew Phelps <ma.phelps@gmail.com>
2018-08-07 15:38:56 +0200

    Style navPage buttons and add bleeding results container holder.
    Will need to refer to use different intpu$objects in bleeding and stroke because the way Shiny is setup

--- ui.R
+++ ui.R
@@ -19,7 +19,8 @@ shinyUI(ui = tagList(
   #
 
   navbarPage(
-    title_txt,
+    title  = title_txt,
+    theme = "www/cars-style.css",
 
     # STROKE ------------------------------------------------------------------
 
@@ -133,7 +134,7 @@ shinyUI(ui = tagList(
                      results_1,
                      fluidRow(
                        wellPanel(
-                         class = "results_stroke_well",
+                         class = "results_well",
                          # div(class = "results_header", results_stroke_header),
                          div(class = "results_header", textOutput("stroke_desc1")),
                          tags$h1(strong(textOutput("enter_age"))),
@@ -148,7 +149,7 @@ shinyUI(ui = tagList(
                      fluidRow(
                        wellPanel(
                          id = "plot_matrix",
-                         class = "results_stroke_well",
+                         class = "results_well",
                          div(class = "output_desc", textOutput("stroke_desc2")),
                          br(),
                          uiOutput("plot_stroke")
@@ -197,103 +198,122 @@ shinyUI(ui = tagList(
 
                )
              )),
-    tabPanel("Blødning",
+    tabPanel(
+      "Blødning",
 
-             # BLEEDING ----------------------------------------------------------------
-             fluidRow(column(
-               id = "input_col",
-               6,
-               wellPanel(
-                 # BLEEDING-INPUT ----------------------------------------------------------
-
-
-                 div(class = "titles", input_title),
-                 br(),
-
-                 numericInput(
-                   inputId = "user_age",
-                   label = in_age,
-                   min = 20,
-                   max = 99,
-                   value = "Age"
-                 ),
-
-                 shinyWidgets::radioGroupButtons(
-                   inputId = "sex",
-                   label = in_sex,
-                   choices = c("Mand" = "no",
-                               "Kvinde" = "yes"),
-                   checkIcon = list(yes = icon("check")),
-                   selected =  "no",
-                   # width = button.width,
-                   justified = TRUE
-                 ),
-
-                 br(),
-                 div(id = "in_prevDiag", in_prevDiag),
-                 br(),
+      # BLEEDING ----------------------------------------------------------------
+      fluidRow(column(
+        id = "input_col",
+        6,
+        wellPanel(
+          # BLEEDING-INPUT ----------------------------------------------------------
 
 
-                 shinyWidgets::radioGroupButtons(
-                   inputId = "stroke",
-                   label = in_stroke,
-                   choiceNames = in_yesNo_choice,
-                   selected =  "no",
-                   choiceValues = c("no",
-                                    "yes"),
-                   checkIcon = list(yes = icon("check")),
-                   # width = button.width,
-                   justified = TRUE
-                 ),
+          div(class = "titles", input_title),
+          br(),
 
-                 shinyWidgets::radioGroupButtons(
-                   inputId = "hyperT",
-                   label = in_hyperT,
-                   choiceNames = in_yesNo_choice,
-                   selected =  ("no"),
-                   choiceValues = c("no",
-                                    "yes"),
-                   checkIcon = list(yes = icon("check")),
-                   # width = button.width,
-                   justified = TRUE
-                 ),
+          numericInput(
+            inputId = "user_age",
+            label = in_age,
+            min = 20,
+            max = 99,
+            value = "Age"
+          ),
 
-                 shinyWidgets::radioGroupButtons(
-                   inputId = "bleeding",
-                   label = in_bleed,
-                   choiceNames = in_yesNo_choice,
-                   selected =  ("no"),
-                   choiceValues = c("no",
-                                    "yes"),
-                   checkIcon = list(yes = icon("check")),
-                   # width = button.width,
-                   justified = TRUE
-                 ),
+          shinyWidgets::radioGroupButtons(
+            inputId = "sex",
+            label = in_sex,
+            choices = c("Mand" = "no",
+                        "Kvinde" = "yes"),
+            checkIcon = list(yes = icon("check")),
+            selected =  "no",
+            # width = button.width,
+            justified = TRUE
+          ),
 
-                 shinyWidgets::radioGroupButtons(
-                   inputId = "ckd",
-                   label = in_ckd,
-                   choiceNames = in_yesNo_choice,
-                   selected =  ("no"),
-                   choiceValues = c("no",
-                                    "yes"),
-                   checkIcon = list(yes = icon("check")),
-                   # width = button.width,
-                   justified = TRUE
-                 ),
+          br(),
+          div(id = "in_prevDiag", in_prevDiag),
+          br(),
 
-                 shinyWidgets::radioGroupButtons(
-                   inputId = "drugs",
-                   label = in_drug,
-                   choiceNames = in_yesNo_choice,
-                   selected =  ("no"),
-                   choiceValues = c("no",
-                                    "yes"),
-                   checkIcon = list(yes = icon("check")),
-                   # width = button.width,
-                   justified = TRUE
-                 )
-               )
-             )))
+
+          shinyWidgets::radioGroupButtons(
+            inputId = "stroke",
+            label = in_stroke,
+            choiceNames = in_yesNo_choice,
+            selected =  "no",
+            choiceValues = c("no",
+                             "yes"),
+            checkIcon = list(yes = icon("check")),
+            # width = button.width,
+            justified = TRUE
+          ),
+
+          shinyWidgets::radioGroupButtons(
+            inputId = "hyperT",
+            label = in_hyperT,
+            choiceNames = in_yesNo_choice,
+            selected =  ("no"),
+            choiceValues = c("no",
+                             "yes"),
+            checkIcon = list(yes = icon("check")),
+            # width = button.width,
+            justified = TRUE
+          ),
+
+          shinyWidgets::radioGroupButtons(
+            inputId = "bleeding",
+            label = in_bleed,
+            choiceNames = in_yesNo_choice,
+            selected =  ("no"),
+            choiceValues = c("no",
+                             "yes"),
+            checkIcon = list(yes = icon("check")),
+            # width = button.width,
+            justified = TRUE
+          ),
+
+          shinyWidgets::radioGroupButtons(
+            inputId = "ckd",
+            label = in_ckd,
+            choiceNames = in_yesNo_choice,
+            selected =  ("no"),
+            choiceValues = c("no",
+                             "yes"),
+            checkIcon = list(yes = icon("check")),
+            # width = button.width,
+            justified = TRUE
+          ),
+
+          shinyWidgets::radioGroupButtons(
+            inputId = "drugs",
+            label = in_drug,
+            choiceNames = in_yesNo_choice,
+            selected =  ("no"),
+            choiceValues = c("no",
+                             "yes"),
+            checkIcon = list(yes = icon("check")),
+            # width = button.width,
+            justified = TRUE
+          )
+        )
+      ),
+      column(id = "bleeding_results",
+             6,
+             tabsetPanel(type = "tabs",
+                         tabPanel(
+                           results_1,
+                           fluidRow(
+                             wellPanel(
+                               class = "results_well",
+                               # div(class = "results_header", results_stroke_header),
+                               div(class = "results_header"),
+
+                               br()
+
+                             )
+                           )
+                         ))
+                         )))
+    )
   )
-))
+)
